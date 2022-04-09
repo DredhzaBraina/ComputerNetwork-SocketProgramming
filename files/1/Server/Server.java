b14c2bd91978fbbfdfd75dafb33c591bd4144f5c
Aulona Limani <94397920+AulonaLimani@users.noreply.github.com>
2022-04-10 00:34:16 +0200

    Update Server.java

--- files/1/Server/Server.java
+++ files/1/Server/Server.java
@@ -86,38 +86,22 @@ public class Server {
         }
     }
 
-    /**
-     * @param fileName
-     * @return The extension type of the file.
-     */
+  
     public static String getFileExtension(String fileName) {
-        // Get the file type by using the last occurence of . (for example aboutMe.txt returns txt).
-        // Will have issues with files like myFile.tar.gz.
         int i = fileName.lastIndexOf('.');
-        // If there is an extension.
         if (i > 0) {
-            // Set the extension to the extension of the filename.
             return fileName.substring(i + 1);
         } else {
             return "No extension found.";
         }
     }
 
-    /**
-     * When the jpanel is clicked a popup shows to say whether the user wants to download
-     * the selected document.
-     *
-     * @return A mouselistener that is used by the jpanel.
-     */
     public static MouseListener getMyMouseListener() {
         return new MouseListener() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                // Get the source of the click which is the JPanel.
                 JPanel jPanel = (JPanel) e.getSource();
-                // Get the ID of the file.
                 int fileId = Integer.parseInt(jPanel.getName());
-                // Loop through the file storage and see which file is the selected one.
                 for (MyFile myFile : myFiles) {
                     if (myFile.getId() == fileId) {
                         JFrame jfPreview = createFrame(myFile.getName(), myFile.getData(), myFile.getFileExtension());
@@ -150,83 +134,53 @@ public class Server {
 
     public static JFrame createFrame(String fileName, byte[] fileData, String fileExtension) {
 
-        // Frame to hold everything.
         JFrame jFrame = new JFrame("WittCode's File Downloader");
         // Set the size of the frame.
         jFrame.setSize(400, 400);
 
-        // Panel to hold everything.
         JPanel jPanel = new JPanel();
-        // Make the layout a box layout with child elements stacked on top of each other.
         jPanel.setLayout(new BoxLayout(jPanel, BoxLayout.Y_AXIS));
 
-        // Title above panel.
         JLabel jlTitle = new JLabel("WittCode's File Downloader");
-        // Center the label title horizontally.
         jlTitle.setAlignmentX(Component.CENTER_ALIGNMENT);
-        // Change the font family, size, and style.
         jlTitle.setFont(new Font("Arial", Font.BOLD, 25));
-        // Add spacing on the top and bottom of the element.
         jlTitle.setBorder(new EmptyBorder(20,0,10,0));
 
-        // Label to prompt the user if they are sure they want to download the file.
         JLabel jlPrompt = new JLabel("Are you sure you want to download " + fileName + "?");
-        // Change the font style, size, and family of the label.
         jlPrompt.setFont(new Font("Arial", Font.BOLD, 20));
-        // Add spacing on the top and bottom of the label.
         jlPrompt.setBorder(new EmptyBorder(20,0,10,0));
-        // Center the label horizontally.
         jlPrompt.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        // Create the yes for accepting the download.
         JButton jbYes = new JButton("Yes");
         jbYes.setPreferredSize(new Dimension(150, 75));
-        // Set the font for the button.
         jbYes.setFont(new Font("Arial", Font.BOLD, 20));
 
-        // No button for rejecting the download.
         JButton jbNo = new JButton("No");
-        // Change the size of the button must be preferred because if not the layout will ignore it.
         jbNo.setPreferredSize(new Dimension(150, 75));
-        // Set the font for the button.
         jbNo.setFont(new Font("Arial", Font.BOLD, 20));
 
-        // Label to hold the content of the file whether it be text of images.
         JLabel jlFileContent = new JLabel();
-        // Align the label horizontally.
         jlFileContent.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        // Panel to hold the yes and no buttons and make the next to each other left and right.
         JPanel jpButtons = new JPanel();
-        // Add spacing around the panel.
         jpButtons.setBorder(new EmptyBorder(20, 0, 10, 0));
-        // Add the yes and no buttons.
         jpButtons.add(jbYes);
         jpButtons.add(jbNo);
 
-        // If the file is a text file then display the text.
         if (fileExtension.equalsIgnoreCase("txt")) {
-            // Wrap it with <html> so that new lines are made.
             jlFileContent.setText("<html>" + new String(fileData) + "</html>");
-            // If the file is not a text file then make it an image.
         } else {
             jlFileContent.setIcon(new ImageIcon(fileData));
         }
 
-        // Yes so download file.
         jbYes.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // Create the file with its name.
                 File fileToDownload = new File(fileName);
                 try {
-                    // Create a stream to write data to the file.
                     FileOutputStream fileOutputStream = new FileOutputStream(fileToDownload);
-                    // Write the actual file data to the file.
                     fileOutputStream.write(fileData);
-                    // Close the stream.
                     fileOutputStream.close();
-                    // Get rid of the jFrame. after the user clicked yes.
                     jFrame.dispose();
                 } catch (IOException ex) {
                     ex.printStackTrace();
@@ -235,25 +189,20 @@ public class Server {
             }
         });
 
-        // No so close window.
         jbNo.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // User clicked no so don't download the file but close the jframe.
                 jFrame.dispose();
             }
         });
 
-        // Add everything to the panel before adding to the frame.
         jPanel.add(jlTitle);
         jPanel.add(jlPrompt);
         jPanel.add(jlFileContent);
         jPanel.add(jpButtons);
 
-        // Add panel to the frame.
         jFrame.add(jPanel);
 
-        // Return the jFrame so it can be passed the right data and then shown.
         return jFrame;
 
     }
