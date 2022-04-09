532f6a4842c506ee5540d6aec45e15486be3a06e
Aulona Limani <94397920+AulonaLimani@users.noreply.github.com>
2022-04-10 00:31:21 +0200

    Update Server.java

--- files/1/Server/Server.java
+++ files/1/Server/Server.java
@@ -38,38 +38,24 @@ public class Server {
         jFrame.add(jScrollPane);
         jFrame.setVisible(true);
 
-        // Create a server socket that the server will be listening with.
         ServerSocket serverSocket = new ServerSocket(1234);
 
-        // This while loop will run forever so the server will never stop unless the application is closed.
         while (true) {
 
             try {
-                // Wait for a client to connect and when they do create a socket to communicate with them.
                 Socket socket = serverSocket.accept();
 
-                // Stream to receive data from the client through the socket.
                 DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
 
-                // Read the size of the file name so know when to stop reading.
                 int fileNameLength = dataInputStream.readInt();
-                // If the file exists
                 if (fileNameLength > 0) {
-                    // Byte array to hold name of file.
                     byte[] fileNameBytes = new byte[fileNameLength];
-                    // Read from the input stream into the byte array.
                     dataInputStream.readFully(fileNameBytes, 0, fileNameBytes.length);
-                    // Create the file name from the byte array.
                     String fileName = new String(fileNameBytes);
-                    // Read how much data to expect for the actual content of the file.
                     int fileContentLength = dataInputStream.readInt();
-                    // If the file exists.
                     if (fileContentLength > 0) {
-                        // Array to hold the file data.
                         byte[] fileContentBytes = new byte[fileContentLength];
-                        // Read from the input stream into the fileContentBytes array.
                         dataInputStream.readFully(fileContentBytes, 0, fileContentBytes.length);
-                        // Panel to hold the picture and file name.
                         JPanel jpFileRow = new JPanel();
                         jpFileRow.setLayout(new BoxLayout(jpFileRow, BoxLayout.X_AXIS));
                         // Set the file name.
@@ -77,28 +63,20 @@ public class Server {
                         jlFileName.setFont(new Font("Arial", Font.BOLD, 20));
                         jlFileName.setBorder(new EmptyBorder(10,0, 10,0));
                         if (getFileExtension(fileName).equalsIgnoreCase("txt")) {
-                            // Set the name to be the fileId so you can get the correct file from the panel.
                             jpFileRow.setName((String.valueOf(fileId)));
                             jpFileRow.addMouseListener(getMyMouseListener());
-                            // Add everything.
                             jpFileRow.add(jlFileName);
                             jPanel.add(jpFileRow);
                             jFrame.validate();
                         } else {
-                            // Set the name to be the fileId so you can get the correct file from the panel.
                             jpFileRow.setName((String.valueOf(fileId)));
-                            // Add a mouse listener so when it is clicked the popup appears.
                             jpFileRow.addMouseListener(getMyMouseListener());
-                            // Add the file name and pic type to the panel and then add panel to parent panel.
                             jpFileRow.add(jlFileName);
                             jPanel.add(jpFileRow);
-                            // Perform a relayout.
                             jFrame.validate();
                         }
 
-                        // Add the new file to the array list which holds all our data.
                         myFiles.add(new MyFile(fileId, fileName, fileContentBytes, getFileExtension(fileName)));
-                        // Increment the fileId for the next file to be received.
                         fileId++;
                     }
                 }
